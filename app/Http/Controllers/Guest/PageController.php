4e2d2ac7c1338f8964801be823c321910b035a61
GiadaTonioni <giada.tonioni@gmail.com>
2023-08-01 17:06:42 +0200

    richiamo al database funzionante

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Comic;
+
 class PageController extends Controller
 {
     public function index(){
+        $comics = Comic::all();
+        dd($comics);
         return view('home');
     }
 }
